2205bba6c23797c911bd1bb3bdc362f47789a19b
Faaiq Ali Khan <faiq.khan@onecarnow.com>
2024-11-03 20:38:03 +0500

    game over screen added

--- lib/screens/muscles_builder_game_screen.dart
+++ lib/screens/muscles_builder_game_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:muscles_builder/games/muscles_builder_game.dart';
+import 'package:muscles_builder/screens/game_over_screen.dart';
 
 final MusclesBuilderGame _musclesBuilderGame = MusclesBuilderGame();
 
@@ -11,6 +12,13 @@ class MusclesBuilderGameScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GameWidget(
       game: _musclesBuilderGame,
+      overlayBuilderMap: {
+        GameOverScreen.id: (BuildContext context, MusclesBuilderGame gameRef) {
+          return GameOverScreen(
+            gameRef: gameRef,
+          );
+        }
+      },
     );
   }
 }
