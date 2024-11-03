d58411cc9a1c488a8489fc95d2777635cefddf13
Faaiq Ali Khan <faiq.khan@onecarnow.com>
2024-11-03 19:50:01 +0500

    vaccine component added with handling of vaccine

--- lib/games/muscles_builder_game.dart
+++ lib/games/muscles_builder_game.dart
@@ -1,21 +1,44 @@
 import 'dart:async';
+import 'dart:math';
 
-import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:muscles_builder/components/player_component.dart';
+import 'package:muscles_builder/components/vaccine_component.dart';
 import 'package:muscles_builder/components/virus_component.dart';
 import 'package:muscles_builder/constants/globals.dart';
 import 'package:muscles_builder/inputs/joystick.dart';
 
 class MusclesBuilderGame extends FlameGame
     with DragCallbacks, HasCollisionDetection {
+  late Timer _timer;
   late PlayerComponent playerComponent;
+
+  int _remainingTime = 30;
+
+  final Random _random = Random();
+
+  // Vaccine components
+  final VaccineComponent _vaccineComponent = VaccineComponent(
+    startPosition: Vector2(200, 200),
+  );
+
+  // Vaccine will give immunity only for 4 seconds
+  int _vaccineImmunityTimer = 4;
+
+  // Timer object
+  late Timer vaccineTimer;
+
+  // Random time for the vaccine to appear
+  late int _vaccineTimerAppearance;
 
   @override
   FutureOr<void> onLoad() async {
     super.onLoad();
+    // Generate random number between 0 to 20
+    _vaccineTimerAppearance = _random.nextInt(_remainingTime - 20) + 20;
 
     // Load all the required audio in cache
     FlameAudio.audioCache.loadAll(
@@ -50,5 +73,46 @@ class MusclesBuilderGame extends FlameGame
 
     // Any collision on the bounds of the view port
     add(ScreenHitbox());
+
+    _timer = Timer(
+      1,
+      repeat: true,
+      onTick: () {
+        if (_remainingTime == 0) {
+          pauseEngine();
+        } else if (_remainingTime == _vaccineTimerAppearance) {
+          add(_vaccineComponent);
+        }
+        _remainingTime -= 1;
+      },
+    );
+    _timer.start();
+
+    vaccineTimer = Timer(
+      1,
+      repeat: true,
+      onTick: () {
+        if (_vaccineImmunityTimer == 0) {
+          playerComponent.removeVaccine(); // Call to remove vaccine function
+          _vaccineImmunityTimer = 4; // Again set the immunity time to 4
+          _vaccineTimerAppearance = 0;
+          vaccineTimer.stop();
+        } else {
+          _vaccineImmunityTimer -= 1;
+        }
+      },
+    );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    _timer.update(dt);
+    if (playerComponent.isVaccinated) {
+      vaccineTimer.update(dt);
+    } else if (_vaccineTimerAppearance == 0 && _remainingTime > 3) {
+      _vaccineTimerAppearance = Random().nextInt(_remainingTime - 3) +
+          3; // New vaccine appearance time
+    }
   }
 }
