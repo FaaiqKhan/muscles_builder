bf68db471637b4b7f39f8651dbdae7902122a433
Faaiq Ali Khan <faiq.khan@onecarnow.com>
2024-11-03 22:59:41 +0500

    font size fix

--- lib/screens/game_over_screen.dart
+++ lib/screens/game_over_screen.dart
@@ -30,43 +30,29 @@ class GameOverScreen extends StatelessWidget {
             const SizedBox(
               height: 200,
             ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  gameRef.reset();
-                  gameRef.resumeEngine();
-                  gameRef.overlays.remove(GameOverScreen.id);
-                },
-                child: const Text(
-                  "Again",
-                  style: TextStyle(fontSize: 25),
-                ),
-              ),
+            ElevatedButton(
+              onPressed: () {
+                gameRef.reset();
+                gameRef.resumeEngine();
+                gameRef.overlays.remove(GameOverScreen.id);
+              },
+              child: const Text("Again"),
             ),
             const SizedBox(
               height: 20,
             ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () async {
-                  await Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (_) => const HomeScreen(),
-                    ),
-                  );
-                  gameRef.reset();
-                  gameRef.resumeEngine();
-                  gameRef.overlays.remove(GameOverScreen.id);
-                },
-                child: const Text(
-                  "Home",
-                  style: TextStyle(fontSize: 25),
-                ),
-              ),
+            ElevatedButton(
+              onPressed: () async {
+                await Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (_) => const HomeScreen(),
+                  ),
+                );
+                gameRef.reset();
+                gameRef.resumeEngine();
+                gameRef.overlays.remove(GameOverScreen.id);
+              },
+              child: const Text("Home"),
             )
           ],
         ),
