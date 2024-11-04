1c73d2abf0d1fb3a9d8ee249e013f4ce0a3c3fd3
Faaiq Ali Khan <faiq.khan@onecarnow.com>
2024-11-05 00:28:13 +0500

    muscles builder game instance every time game starts

--- lib/screens/muscles_builder_game_screen.dart
+++ lib/screens/muscles_builder_game_screen.dart
@@ -3,15 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:muscles_builder/games/muscles_builder_game.dart';
 import 'package:muscles_builder/screens/game_over_screen.dart';
 
-final MusclesBuilderGame _musclesBuilderGame = MusclesBuilderGame();
-
 class MusclesBuilderGameScreen extends StatelessWidget {
   const MusclesBuilderGameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GameWidget(
-      game: _musclesBuilderGame,
+      game: MusclesBuilderGame(),
       overlayBuilderMap: {
         GameOverScreen.id: (BuildContext context, MusclesBuilderGame gameRef) {
           return GameOverScreen(
