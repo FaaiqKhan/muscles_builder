51acd8a99e243ae19e9c18df59400fe4570a788b
FaaiqKhan <faiqalikhan49@gmail.com>
2025-03-08 23:36:22 +0100

    code refactoring and using future instead of stream

--- packages/data/lib/models/user_model.dart
+++ packages/data/lib/models/user_model.dart
@@ -1,29 +1,31 @@
 class UserModel {
   const UserModel({
-    required this.uId,
+    this.uId,
     this.email,
     this.displayName,
     this.photoUrl,
     this.phoneNumber,
-    required this.isAnonymous,
-    required this.isEmailVerified,
+    this.isAnonymous,
+    this.isEmailVerified,
     this.providerId,
     this.refreshToken,
-    required this.creationTimestamp,
-    required this.lastSignInTimestamp,
-    required this.isNewUser,
+    this.creationTimestamp,
+    this.lastSignInTimestamp,
+    this.isNewUser,
+    this.accessToken,
   });
 
-  final String uId;
+  final String? uId;
   final String? email;
   final String? displayName;
   final String? photoUrl;
   final String? phoneNumber;
-  final bool isAnonymous;
-  final bool isEmailVerified;
+  final bool? isAnonymous;
+  final bool? isEmailVerified;
   final String? providerId;
   final String? refreshToken;
   final int? creationTimestamp;
   final int? lastSignInTimestamp;
-  final bool isNewUser;
+  final bool? isNewUser;
+  final String? accessToken;
 }
--- packages/domain/lib/domain.dart
+++ packages/domain/lib/domain.dart
@@ -4,5 +4,9 @@ export "entities/user_entity.dart";
 
 export "exceptions/muscles_builder_exceptions.dart";
 
+export "extensions/user_model_extensions.dart";
+
+export "repositories/user_authentication_repository.dart";
+
 export "usecases/user_authentication_usecase.dart";
 export "usecases/user_profile_usecase.dart";
