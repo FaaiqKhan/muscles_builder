c48478cc03dbdfd6af717cdfb6358077fce06d2f
FaaiqKhan <faiqalikhan49@gmail.com>
2025-07-30 12:39:01 +0200

    pause button component refactor

--- lib/components/pause_button_component.dart
+++ lib/components/pause_button_component.dart
@@ -3,36 +3,14 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:muscles_builder/constants/globals.dart';
-import 'package:muscles_builder/constants/spacings.dart';
 import 'package:muscles_builder/games/muscles_builder_game.dart';
 import 'package:muscles_builder/screens/game_pause_screen.dart';
 
 class PauseButtonComponent extends SpriteComponent
-    with HasGameRef<MusclesBuilderGame>, TapCallbacks {
-  void pauseGame() {
-    gameRef.pauseEngine();
-    gameRef.timer.pause();
-    if (gameRef.vaccineTimer.isRunning()) {
-      gameRef.vaccineTimer.pause();
-    }
-    if (gameRef.warmupTimer.isRunning()) {
-      gameRef.proteinTimer.pause();
-    }
-    if (gameRef.warmupTimer.isRunning()) {
-      gameRef.warmupTimer.pause();
-    }
-    gameRef.overlays.add(GamePauseScreen.id);
-  }
-
+    with HasGameReference<MusclesBuilderGame>, TapCallbacks {
   @override
   FutureOr<void> onLoad() async {
-    sprite = await gameRef.loadSprite(Globals.pauseButtonSprite);
-    size = Vector2.all(44);
-    anchor = Anchor.topRight;
-    position = Vector2(
-      gameRef.size.x - Spacings.contentSpacingOf12,
-      gameRef.size.y * 0.01,
-    );
+    sprite = await game.loadSprite(Globals.pauseButtonSprite);
     return await super.onLoad();
   }
 
@@ -40,5 +18,13 @@ class PauseButtonComponent extends SpriteComponent
   void onTapDown(TapDownEvent event) {
     pauseGame();
     super.onTapDown(event);
+  }
+
+  void pauseGame() {
+    game.pauseEngine();
+    if (game.vaccineTimer.isRunning()) {
+      game.vaccineTimer.pause();
+    }
+    game.overlays.add(GamePauseScreen.id);
   }
 }
